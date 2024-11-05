e4bd9ba4a834bfc743c903f6eab80783fb1091a5
kiel-dummy <vinceandisip12@gmail.com>
2024-11-05 15:13:32 +0800

    more api and fixes

--- src/api/api.js
+++ src/api/api.js
@@ -378,3 +378,267 @@ export async function fecthMockSubscriberTrend(subscriber_id, start_date, end_da
   }
 
 }
+
+
+export async function fecthMockSubscriberTimeline(subscriber_id, start_date, end_date) {
+  // new. health: overall metric average
+
+  const mockResponse = {
+    success: true,
+    data: {
+      items: [
+        {
+          "date_range": "2024-10-01",
+          "health": "76.0580762703404",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 88,
+          "total_cbl": 53,
+          "total_booked": 14,
+          "total_cil": 4,
+          "total_crl": 2,
+          "over_total_distribution": "0.81481481481481481481",
+          "over_total_cbl": "0.49074074074074074074",
+          "over_total_booked": "0.12962962962962962963",
+          "over_total_cil": "0.03703703703703703704",
+          "over_total_crl": "0.01851851851851851852"
+        },
+        {
+          "date_range": "2024-10-02",
+          "health": "78.4448653198652",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 71,
+          "total_cbl": 48,
+          "total_booked": 11,
+          "total_cil": 5,
+          "total_crl": 3,
+          "over_total_distribution": "0.65740740740740740741",
+          "over_total_cbl": "0.44444444444444444444",
+          "over_total_booked": "0.10185185185185185185",
+          "over_total_cil": "0.04629629629629629630",
+          "over_total_crl": "0.02777777777777777778"
+        },
+        {
+          "date_range": "2024-10-03",
+          "health": "77.44708994708992",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 50,
+          "total_cbl": 40,
+          "total_booked": 14,
+          "total_cil": 6,
+          "total_crl": 6,
+          "over_total_distribution": "0.46296296296296296296",
+          "over_total_cbl": "0.37037037037037037037",
+          "over_total_booked": "0.12962962962962962963",
+          "over_total_cil": "0.05555555555555555556",
+          "over_total_crl": "0.05555555555555555556"
+        },
+        {
+          "date_range": "2024-10-04",
+          "health": "81.5530303030303",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 82,
+          "total_cbl": 55,
+          "total_booked": 15,
+          "total_cil": 6,
+          "total_crl": 6,
+          "over_total_distribution": "0.75925925925925925926",
+          "over_total_cbl": "0.50925925925925925926",
+          "over_total_booked": "0.13888888888888888889",
+          "over_total_cil": "0.05555555555555555556",
+          "over_total_crl": "0.05555555555555555556"
+        },
+        {
+          "date_range": "2024-10-05",
+          "health": "59.07407407407407",
+          "slots": 0,
+          "distrib": "108",
+          "total_distribution": 16,
+          "total_cbl": 12,
+          "total_booked": 2,
+          "total_cil": 1,
+          "total_crl": 1,
+          "over_total_distribution": "0.14814814814814814815",
+          "over_total_cbl": "0.11111111111111111111",
+          "over_total_booked": "0.01851851851851851852",
+          "over_total_cil": "0.00925925925925925926",
+          "over_total_crl": "0.00925925925925925926"
+        },
+        {
+          "date_range": "2024-10-06",
+          "health": "86.34259259259258",
+          "slots": 0,
+          "distrib": "108",
+          "total_distribution": 17,
+          "total_cbl": 9,
+          "total_booked": 2,
+          "total_cil": 2,
+          "total_crl": 1,
+          "over_total_distribution": "0.15740740740740740741",
+          "over_total_cbl": "0.08333333333333333333",
+          "over_total_booked": "0.01851851851851851852",
+          "over_total_cil": "0.01851851851851851852",
+          "over_total_crl": "0.00925925925925925926"
+        },
+        {
+          "date_range": "2024-10-07",
+          "health": "64.6003898635478",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 69,
+          "total_cbl": 38,
+          "total_booked": 8,
+          "total_cil": 2,
+          "total_crl": 4,
+          "over_total_distribution": "0.63888888888888888889",
+          "over_total_cbl": "0.35185185185185185185",
+          "over_total_booked": "0.07407407407407407407",
+          "over_total_cil": "0.01851851851851851852",
+          "over_total_crl": "0.03703703703703703704"
+        },
+        {
+          "date_range": "2024-10-08",
+          "health": "58.73303427651245",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 72,
+          "total_cbl": 46,
+          "total_booked": 7,
+          "total_cil": 1,
+          "total_crl": 1,
+          "over_total_distribution": "0.66666666666666666667",
+          "over_total_cbl": "0.42592592592592592593",
+          "over_total_booked": "0.06481481481481481481",
+          "over_total_cil": "0.00925925925925925926",
+          "over_total_crl": "0.00925925925925925926"
+        },
+        {
+          "date_range": "2024-10-09",
+          "health": "75.3009259259260",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 62,
+          "total_cbl": 45,
+          "total_booked": 16,
+          "total_cil": 5,
+          "total_crl": 4,
+          "over_total_distribution": "0.57407407407407407407",
+          "over_total_cbl": "0.41666666666666666667",
+          "over_total_booked": "0.14814814814814814815",
+          "over_total_cil": "0.04629629629629629630",
+          "over_total_crl": "0.03703703703703703704"
+        },
+        {
+          "date_range": "2024-10-10",
+          "health": "83.6382890988154",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 84,
+          "total_cbl": 65,
+          "total_booked": 19,
+          "total_cil": 7,
+          "total_crl": 8,
+          "over_total_distribution": "0.77777777777777777778",
+          "over_total_cbl": "0.60185185185185185185",
+          "over_total_booked": "0.17592592592592592593",
+          "over_total_cil": "0.06481481481481481481",
+          "over_total_crl": "0.07407407407407407407"
+        },
+        {
+          "date_range": "2024-10-11",
+          "health": "61.7667958656331",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 55,
+          "total_cbl": 43,
+          "total_booked": 10,
+          "total_cil": 2,
+          "total_crl": 1,
+          "over_total_distribution": "0.50925925925925925926",
+          "over_total_cbl": "0.39814814814814814815",
+          "over_total_booked": "0.09259259259259259259",
+          "over_total_cil": "0.01851851851851851852",
+          "over_total_crl": "0.00925925925925925926"
+        },
+        {
+          "date_range": "2024-10-12",
+          "health": "66.30698005698005",
+          "slots": 0,
+          "distrib": "108",
+          "total_distribution": 49,
+          "total_cbl": 39,
+          "total_booked": 13,
+          "total_cil": 3,
+          "total_crl": 3,
+          "over_total_distribution": "0.45370370370370370370",
+          "over_total_cbl": "0.36111111111111111111",
+          "over_total_booked": "0.12037037037037037037",
+          "over_total_cil": "0.02777777777777777778",
+          "over_total_crl": "0.02777777777777777778"
+        },
+        {
+          "date_range": "2024-10-13",
+          "health": "69.07407407407404",
+          "slots": 0,
+          "distrib": "108",
+          "total_distribution": 10,
+          "total_cbl": 6,
+          "total_booked": 4,
+          "total_cil": 1,
+          "total_crl": 2,
+          "over_total_distribution": "0.09259259259259259259",
+          "over_total_cbl": "0.05555555555555555556",
+          "over_total_booked": "0.03703703703703703704",
+          "over_total_cil": "0.00925925925925925926",
+          "over_total_crl": "0.01851851851851851852"
+        },
+        {
+          "date_range": "2024-10-14",
+          "health": "79.35185185185183",
+          "slots": 1,
+          "distrib": "108",
+          "total_distribution": 43,
+          "total_cbl": 18,
+          "total_booked": 8,
+          "total_cil": 4,
+          "total_crl": 0,
+          "over_total_distribution": "0.39814814814814814815",
+          "over_total_cbl": "0.16666666666666666667",
+          "over_total_booked": "0.07407407407407407407",
+          "over_total_cil": "0.03703703703703703704",
+          "over_total_crl": "0.00000000000000000000"
+        },
+        {
+          "date_range": "2024-10-15",
+          "health": null,
+          "slots": 1,
+          "distrib": "0",
+          "total_distribution": 0,
+          "total_cbl": 0,
+          "total_booked": 0,
+          "total_cil": 0,
+          "total_crl": 0,
+          "over_total_distribution": null,
+          "over_total_cbl": null,
+          "over_total_booked": null,
+          "over_total_cil": null,
+          "over_total_crl": null
+        }
+      ]
+    }
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  // Return mock data
+  if (mockResponse.success) {
+    return {
+      data: mockResponse
+    };
+  } else {
+    throw new Error("Failed to fetch subscribers");
+  }
+}
